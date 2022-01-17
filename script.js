c2af803197d9d83677547c2d8c26c72ff23bbba1
euphroshub <euphros@protonmail.com>
2022-01-17 13:10:17 -0500

    wip

--- script.js
+++ script.js
@@ -260,7 +260,7 @@ console.log('Test end'); // 2
 
 
 // Building promises - takes 1 argument (executor func)
-const lotteryPromise = new Promise(function(resolve, reject) {
+/* const lotteryPromise = new Promise(function(resolve, reject) {
     console.log('Lottery draw is happening.')
     setTimeout(function() {
         if (Math.random() >= 0.5) {
@@ -292,4 +292,53 @@ wait(2).then(() => {
 
 // static method on the promise constructor - resolve immediatly
 Promise.resolve('abc').then(x => console.log(x));
-Promise.reject(new Error('Panic')).catch(x => console.error(x));
+Promise.reject(new Error('Panic')).catch(x => console.error(x)); */
+
+////////////////////////////////////////////////////////////////////////////////////
+
+
+console.log('Getting position');
+
+const getPosition = function() {
+     return new Promise(function(resolve, reject) {
+        /*
+        navigator.geolocation.getCurrentPosition(
+            position => resolve(position),
+            err => reject(err)
+        );
+        */
+       // does the same as the commented block up
+       navigator.geolocation.getCurrentPosition(resolve, reject);
+    }); 
+}
+
+//getPosition().then(pos => console.log(pos));
+
+const whereAmI = function () {
+
+    getPosition()
+        .then(pos => {
+            const {latitude: lat, longitude: lng} = pos.coords;
+
+            return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+        })
+      .then(res => {
+        if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+        console.log(`You are in ${data.city}, ${data.country}`);
+  
+        return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+      })
+      .then(res => {
+        if (!res.ok) throw new Error(`Country not found (${res.status})`);
+  
+        return res.json();
+      })
+      .then(data => renderCountry(data[0]))
+      .catch(err => console.error(`${err.message} 💥`));
+  };
+
+  btn.addEventListener('click', whereAmI);
