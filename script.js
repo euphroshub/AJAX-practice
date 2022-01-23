39837a476386a8a3e5e449083858a8e21107c362
euphroshub <euphros@protonmail.com>
2022-01-23 12:10:46 -0500

    coding challenge 2

--- script.js
+++ script.js
@@ -297,10 +297,10 @@ Promise.reject(new Error('Panic')).catch(x => console.error(x)); */
 ////////////////////////////////////////////////////////////////////////////////////
 
 
-console.log('Getting position');
+/* console.log('Getting position');
 
 const getPosition = function() {
-     return new Promise(function(resolve, reject) {
+     return new Promise(function(resolve, reject) { */
         /*
         navigator.geolocation.getCurrentPosition(
             position => resolve(position),
@@ -308,13 +308,13 @@ const getPosition = function() {
         );
         */
        // does the same as the commented block up
-       navigator.geolocation.getCurrentPosition(resolve, reject);
+/*        navigator.geolocation.getCurrentPosition(resolve, reject);
     }); 
-}
+} */
 
 //getPosition().then(pos => console.log(pos));
 
-const whereAmI = function () {
+/* const whereAmI = function () {
 
     getPosition()
         .then(pos => {
@@ -341,4 +341,74 @@ const whereAmI = function () {
       .catch(err => console.error(`${err.message} 💥`));
   };
 
-  btn.addEventListener('click', whereAmI);
+  btn.addEventListener('click', whereAmI); */
+
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Consume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+
+*/
+const wait = function(seconds) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, seconds * 1000);
+    });
+}
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function(imgPath) {
+    return new Promise(function(resolve, reject) {
+        const img = document.createElement('img');
+        img.src = imgPath;
+
+        img.addEventListener('load', function() {
+            imgContainer.append(img);
+            resolve(img);
+        });
+
+        img.addEventListener('error', function() {
+            reject(new Error('Image not found'));
+        });
+    });
+}
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+    .then(img => {
+    currentImg = img;
+    console.log('Image 1 has been loaded');
+    return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+        return createImage('img/img-2.jpg');
+    })
+    .then(img => {
+        currentImg = img;
+        console.log('Image 2 has been loaded');
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+    })
+    .catch(err => console.error(err));
